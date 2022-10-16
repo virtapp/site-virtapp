70b7452e9acee2422fd7e8a7e8780f8e6c6abc6b
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-10-16 21:38:54 +0300

    update

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'fuko09phsurxho');
 
 /** MySQL hostname */
-define('DB_HOST', 'swarmdb');
+define('DB_HOST', 'mariadb-mariadb-galera');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
